1d98ccc02920ce1731b84811d7a7affecf3fd90e
Bright <bright@Brights-MacBook-Air.local>
2026-08-10 10:25:09 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
           <main className="min-h-screen">{children}</main>
           <Footer />
         </AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
